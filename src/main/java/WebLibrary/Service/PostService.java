3d4fac0bead520c0d3a2414cd942626313675b0b
ramilmm <ramilmmk@gmail.com>
2017-11-10 18:15:13 +0300

    schedule installed

--- src/main/java/WebLibrary/Service/PostService.java
+++ src/main/java/WebLibrary/Service/PostService.java
@@ -10,7 +10,6 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
